58a9f4495086dd7e4da70c374c9f4d6012d82292
paulo-mesquita <pauloclarinete1@hotmail.com>
2020-02-23 23:42:13 -0300

    criação de notebook

--- python/data-science/amostragem/amostra1.py
+++ python/data-science/amostragem/amostra1.py
@@ -1,5 +1,5 @@
 import pandas as pd
 import numpy as np
 
-base = pd.read_csv('/home/haboryn/Programacao/PYTHON/vscode/RecursosdoCurso/Dados/iris.csv')
-base
+base1 = pd.read_csv('/home/haboryn/Programacao/PYTHON/vscode/RecursosdoCurso/Dados/iris.csv')
+base1
